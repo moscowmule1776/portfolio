ab3577030723c9e2c860d1965bfceafb1c806ce0
moscowmule1776 <114514397+moscowmule1776@users.noreply.github.com>
2022-12-06 16:10:18 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 function showHidden(className) {
+  var split = className.split("_")
+
   var ele = document.getElementById(className);
-    if(ele.style.display == "block") {
-            ele.style.display = "none";
-    } else {
-        ele.style.display = "block";
+  var col = document.getElementById(split[0]);
+
+  if (ele.style.display == "block") {
+    ele.style.display = "none";
+    col.style.color = "rgba(255, 255, 255, 0.5)";
+    col.style.outline = "4px solid rgba(255, 255, 255, 0.25)";
+    col.className = "project_card";
+  } else {
+    ele.style.display = "block";
+    col.className = "project_card_expand"
+    col.style.color = "#E2734A";
+    col.style.outline = "4px solid #E2734A";
+    document.getElementById("race_car_" + split[0]).className = "race_car_" + split[0] + "_animate";
+  }
+}
+
+function showAllHidden() {
+  var divs = ["dev_hidden", "eth_hidden", "id_hidden", "ps_hidden"]
+
+  for (var i = 0; i < 4; i++) {
+    var ele = document.getElementById(divs[i]);
+    var col = document.getElementById(divs[i].split("_")[0]);
+
+    if (ele.style.display != "block") {
+      ele.style.display = "block";
+      col.className = "project_card_expand"
+      col.style.color = "#E2734A";
+      col.style.outline = "4px solid #E2734A";
     }
+  }
+}
+
+function hideAllShown() {
+  var divs = ["dev_hidden", "eth_hidden", "id_hidden", "ps_hidden"]
+
+  for (var i = 0; i < 4; i++) {
+    var ele = document.getElementById(divs[i]);
+    var col = document.getElementById(divs[i].split("_")[0]);
+
+    if (ele.style.display == "block") {
+      ele.style.display = "none";
+      col.style.color = "rgba(255, 255, 255, 0.5)";
+      col.style.outline = "4px solid rgba(255, 255, 255, 0.25)";
+      col.className = "project_card";
+    }
+  }
+}
+
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
